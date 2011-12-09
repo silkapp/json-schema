b02cd90880c26fa4185bd29ddabc76bb3275708f
Erik Hesselink <erik@typlab.com>
2011-12-09 13:12:04 +0100

    Export Proxy from Data.JSON.Schema for convenience.

--- src/Data/JSON/Schema.hs
+++ src/Data/JSON/Schema.hs
@@ -1,7 +1,9 @@
 module Data.JSON.Schema 
   ( module Data.JSON.Schema.Generic
   , module Data.JSON.Schema.Types
+  , Proxy (..)
   ) where
 
 import Data.JSON.Schema.Generic
 import Data.JSON.Schema.Types
+import Data.Proxy
