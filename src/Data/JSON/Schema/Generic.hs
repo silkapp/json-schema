2cdd954092e19ce451ab774ac94f0ee898cde359
Thomas van Noort <thomas@silkapp.com>
2012-10-31 15:32:40 +0100

    Json-schema: Special case for datatype with constants only.

--- src/Data/JSON/Schema/Generic.hs
+++ src/Data/JSON/Schema/Generic.hs
@@ -9,6 +9,7 @@
 module Data.JSON.Schema.Generic (gSchema) where
 
 import Generics.Regular
+import Generics.Regular.IsEnum
 import Control.Applicative hiding (empty, (<|>))
 import Data.Char
 import Data.Maybe
@@ -17,7 +18,7 @@ import Data.JSON.Schema.Combinators
 import Data.Proxy
 
 class GJSONSCHEMA f where
-  gSchema' :: Bool -> (Proxy a -> Schema) -> Proxy (f a) -> Schema
+  gSchema' :: Bool -> Bool -> (Proxy a -> Schema) -> Proxy (f a) -> Schema
 
 -- Recursive positions disabled for now, it casauses infintite data strcutres. This is a problem to be solved!
 {-
@@ -26,21 +27,23 @@ instance GJSONSCHEMA I where
 -}
 
 instance JSONSchema a => GJSONSCHEMA (K a) where
-  gSchema' _ _ = schema . fmap unK
+  gSchema' _ _ _ = schema . fmap unK
 
 instance GJSONSCHEMA (K String) where
-  gSchema' _ _ _ = Value 0 (-1)
+  gSchema' _ _ _ _ = Value 0 (-1)
 
 instance GJSONSCHEMA U where
-  gSchema' _ _ _ = empty
+  gSchema' _ _ _ _ = empty
 
 instance (GJSONSCHEMA f, GJSONSCHEMA g) => GJSONSCHEMA (f :+: g) where
-  gSchema' mc f p = gSchema' mc f (gL <$> p)
-                <|> gSchema' mc f (gR <$> p)
-            where gL :: (f :+: g) r -> f r
-                  gL _ = undefined
-                  gR :: (f :+: g) r -> g r
-                  gR _ = undefined
+  gSchema' mc enm f p =
+        gSchema' mc enm f (gL <$> p)
+    <|> gSchema' mc enm f (gR <$> p)
+    where
+      gL :: (f :+: g) r -> f r
+      gL _ = undefined
+      gR :: (f :+: g) r -> g r
+      gR _ = undefined
 
 gFst :: (f :*: g) r -> f r
 gFst (f :*: _) = f
@@ -49,10 +52,11 @@ gSnd :: (f :*: g) r -> g r
 gSnd (_ :*: g) = g
 
 instance (GJSONSCHEMA f, GJSONSCHEMA g) => GJSONSCHEMA (f :*: g) where
-  gSchema' mc f p = gSchema' mc f (gFst <$> p) `merge` gSchema' mc f (gSnd <$> p)
+  gSchema' mc enm f p = gSchema' mc enm f (gFst <$> p) `merge` gSchema' mc enm f (gSnd <$> p)
 
 instance (Constructor c, GJSONSCHEMA f) => GJSONSCHEMA (C c f) where
-  gSchema' mc f = wrap . gSchema' mc f . fmap unC
+  gSchema' mc True f = const $ Value 0 (-1)
+  gSchema' mc enm f = wrap . gSchema' mc enm f . fmap unC
     where
       wrap = if mc
              then field (firstLetterToLower $ conName (undefined :: C c f r)) True
@@ -63,18 +67,18 @@ firstLetterToLower ""     = ""
 firstLetterToLower (l:ls) = toLower l : ls
 
 instance (Selector s, JSONSchema a) => GJSONSCHEMA (S s (K (Maybe a))) where
-  gSchema' _ _ = field (selName (undefined :: S s f r)) False . schema . fmap (fromJust . unK . unS)
+  gSchema' _ _ _ = field (selName (undefined :: S s f r)) False . schema . fmap (fromJust . unK . unS)
 
 instance Selector s => GJSONSCHEMA (S s (K (Maybe String))) where
-  gSchema' _ _ _ = field (selName (undefined :: S s f r)) False $ Value 0 (-1)
+  gSchema' _ _ _ _ = field (selName (undefined :: S s f r)) False $ Value 0 (-1)
 
 instance (Selector s, GJSONSCHEMA f) => GJSONSCHEMA (S s f) where
-  gSchema' mc f = field (selName (undefined :: S s f r)) True . gSchema' mc f . fmap unS
+  gSchema' mc enm f = field (selName (undefined :: S s f r)) True . gSchema' mc enm f . fmap unS
 
 multipleConstructors :: (Regular a, ConNames (PF a)) => Proxy a -> Bool
 multipleConstructors = (> 1) . length . conNames . pv
   where pv :: Proxy a -> a
         pv _ = undefined
 
-gSchema :: (Regular a, GJSONSCHEMA (PF a), ConNames (PF a)) => Proxy a -> Schema
-gSchema p = gSchema' (multipleConstructors p) gSchema (fmap from p)
+gSchema :: (Regular a, GJSONSCHEMA (PF a), GIsEnum (PF a), ConNames (PF a)) => Proxy a -> Schema
+gSchema p = gSchema' (multipleConstructors p) (isEnum p) gSchema (fmap from p)
