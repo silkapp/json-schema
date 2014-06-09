9f7b3fc392425e4c944362a1d53f7ba1fadab28c
Adam Bergmark <adam@bergmark.nl>
2014-06-09 18:38:31 +0200

    Abstract pack calls (closes #5)

--- src/Data/JSON/Schema/Generic.hs
+++ src/Data/JSON/Schema/Generic.hs
@@ -71,12 +71,12 @@ instance (GJSONSCHEMA f, GJSONSCHEMA g) => GJSONSCHEMA (f :*: g) where
   gSchema' enm names p = gSchema' enm names (gFst <$> p) `merge` gSchema' enm names (gSnd <$> p)
 
 instance (Constructor c, GJSONSCHEMA f) => GJSONSCHEMA (M1 C c f) where
-  gSchema' True _ = toConstant . T.pack . conName . pv
+  gSchema' True _ = toConstant . conNameT . pv
     where
   gSchema' enm names = wrap . gSchema' enm names . fmap unM1
     where
       wrap = if multipleConstructors names
-             then field (firstLetterToLower . T.pack $ conName (undefined :: M1 C c f p)) True
+             then field (firstLetterToLower $ conNameT (undefined :: M1 C c f p)) True
              else id
 
 instance GJSONSCHEMA f => GJSONSCHEMA (M1 D c f) where
@@ -89,11 +89,11 @@ firstLetterToLower m = case T.uncons m of
   Just (l, ls) -> T.cons (toLower l) ls
 
 instance (Selector c, JSONSchema a) => GJSONSCHEMA (M1 S c (K1 i (Maybe a))) where
-  gSchema' _ _ = field ((T.pack . selName) (undefined :: M1 S c f p)) False . schema . fmap (fromJust . unK1 . unM1)
+  gSchema' _ _ = field (selNameT (undefined :: M1 S c f p)) False . schema . fmap (fromJust . unK1 . unM1)
 
 -- TODO This instance does not correspond to the generic-aeson representation for Maybe
 instance Selector c => GJSONSCHEMA (M1 S c (K1 i (Maybe String))) where
-  gSchema' _ _ _ = field ((T.pack . selName) (undefined :: M1 S c f p)) False $ Value unboundedLength
+  gSchema' _ _ _ = field (selNameT (undefined :: M1 S c f p)) False $ Value unboundedLength
 
 instance (Selector c, GJSONSCHEMA f) => GJSONSCHEMA (M1 S c f) where
   gSchema' enm names = wrap . gSchema' enm names . fmap unM1
@@ -101,6 +101,12 @@ instance (Selector c, GJSONSCHEMA f) => GJSONSCHEMA (M1 S c f) where
       wrap = case (T.pack . selName) (undefined :: M1 S c f p) of
         "" -> id
         s -> field s True
+
+conNameT :: forall c (t :: * -> (* -> *) -> * -> *) (f :: * -> *) a. Constructor c => t c f a -> Text
+conNameT x = T.pack . conName $ x
+
+selNameT :: forall s (t :: * -> (* -> *) -> * -> *) (f :: * -> *) a. Selector s => t s f a -> Text
+selNameT x = T.pack . selName $ x
 
 multipleConstructors :: [Text] -> Bool
 multipleConstructors = (> 1) . length
